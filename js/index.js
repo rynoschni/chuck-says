f669df521fa6ba9006c00a6320af73b451336989
Ryan Schniederjan <ryno@schnietke.com>
2020-08-25 11:48:04 -0500

    added dropdown and categories

--- js/index.js
+++ js/index.js
@@ -3,27 +3,45 @@ const chuckSays = document.getElementById('chuckSays');
 const getButton = document.getElementById('refreshQuote');
 const submitFormButton = document.getElementById('submitForm');
 
-const defaultCategory = "dev"
+
+let category = "dev"
 // function getQuote(category) {}
-const getQuote = (category) => {
+const getQuote = () => {
     const url = `https://api.chucknorris.io/jokes/random?category=${category}`;
     get(url).then(function(fetchResponse){
         chuckSays.innerHTML = fetchResponse.value;
     });
 };
 
+const getCategories = () => {
+    const url = `https://api.chucknorris.io/jokes/categories`;
+    const dropDownMenu = document.getElementById('categoryInput');
+    get(url).then(function(fetchCategories){
+        fetchCategories
+            .filter(category => category !== 'explicit' && category !== 'political' && category !== 'religion')
+            .map(function(category){
+            const catOption = document.createElement('option');
+            catOption.value = category;
+            catOption.text = category;
+            dropDownMenu.appendChild(catOption);
+        });
+    });
+};
+
 getButton.addEventListener('click',function(event){
     event.preventDefault();
-    getQuote(defaultCategory);
+    getQuote();
 });
 
-submitFormButton.addEventListener('click', function(e){
+const getChuckQuotes = document.getElementById('getChuckQuote');
+getChuckQuotes.addEventListener('submit', e => {
     e.preventDefault();
     const userInput = document.getElementById('categoryInput');
-    const category = userInput.value;
-    getQuote(category);
+    category = userInput.value;
+    getQuote();
 });
 
 (function(){
-    getQuote(defaultCategory)
+    getCategories();
+    getQuote()
 })(); //IIFE - Immediate Invoked Function Expression  function wrapped in Para, followed by Para
